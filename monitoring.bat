322448e9876e72fb5f7634de7f5041b4d557838c
LibyX13 <stanokakseli2023@gmail.com>
2026-07-02 14:34:34 +0300

    Add files via upload

---
+++ monitoring.bat
@@ -0,0 +1,8 @@
+@echo off
+start "" "https://gamemonitoring.ru/counter-strike-2/servers/6896207/vote"
+start "" "https://gamemonitoring.ru/counter-strike-2/servers/4958800/vote"
+start "" "https://gamemonitoring.ru/counter-strike-2/servers/6360833/vote"
+start "" "https://gamemonitoring.ru/counter-strike-2/servers/4803387/vote"
+start "" "https://gamemonitoring.ru/counter-strike-2/servers/7206214/vote"
+start "" "https://gamemonitoring.ru/counter-strike-2/servers/7907581/vote"
+start "" "https://gamemonitoring.ru/counter-strike-global-offensive/servers/11221570/vote"
